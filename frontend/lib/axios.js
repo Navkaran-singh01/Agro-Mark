04c655a586d858b6824da4591f4683457a947087
Navkaran-singh01 <manveerrandhawa0055@gmail.com>
2025-11-24 19:20:52 +0530

    Fix: Removed deprecated MongoDB connection options

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://agro-mark.onrender.com",
     withCredentials:true,
 })
