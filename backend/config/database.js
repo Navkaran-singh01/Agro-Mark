1ea5c0014abe8c10f6a36eb089400e6b7cc1f746
Navkaran-singh01 <manveerrandhawa0055@gmail.com>
2025-11-24 18:49:43 +0530

    Fix: Removed deprecated MongoDB connection options

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,13 @@ const mongoose=require("mongoose");
 require('dotenv').config();
 
 exports.connectDB=async()=>{
-try{
-    await mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    });
-    console.log("database connected");
-}catch(error){
+    try{
+        // NO OPTIONS NEEDED HERE FOR MODERN MONGOOSE
+        await mongoose.connect(process.env.MONGO_URI); 
+
+        console.log("database connected");
+    }catch(error){
         console.error("Database connection failed. Error:", error.message);
         process.exit(1);
-}
+    }
 }
